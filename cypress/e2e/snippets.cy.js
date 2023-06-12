497fa691467d4a7dd9f0aaf54e56157b6a42c394
Whitney Lea <whitslea@gmail.com>
2023-06-12 15:57:04 -0400

    updated video URL, creative title and fully generates snippet

--- cypress/e2e/snippets.cy.js
+++ cypress/e2e/snippets.cy.js
@@ -15,12 +15,13 @@ context ('Snippets', () => {
             cy.get('#vast-dropdown').click();
             cy.visit('https://video-tag-builder.kargo.com/vast-instream');
             cy.get('.btn').contains('URL').click(); // Toggle Video URL button from Upload to URL
-            cy.get(':nth-child(3) > div.my-2 > .form-control').type('https://springhillcompany.com'); // Type valid https
-            cy.get('.row > :nth-child(1) > :nth-child(1) > .form-control').type('Go Hawks'); // Type creative title
+            cy.get(':nth-child(3) > div.my-2 > .form-control').type('https://media.giphy.com/media/3HG7iF94VgtXBjwhAT/giphy.mp4'); // Type valid https
+            cy.get('.row > :nth-child(1) > :nth-child(1) > .form-control').type('Spiderman'); // Type creative title
             cy.get('.col-12 > :nth-child(1) > .form-control').type('nba.com'); // Type valid video clickthrough URL
             cy.scrollTo('bottom');
             cy.contains('Generate VAST').click(); // Submit form
-            cy.scrollTo('top');
+            cy.wait(2000)
+            cy.contains('Generate VAST').click();
         });
     });
 });
